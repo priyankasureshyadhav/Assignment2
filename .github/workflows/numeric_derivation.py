e024292cc198cdc90e81dd2974287b97965ac5a1
Priyanka Yadhav Suresh <122357782+priyankasureshyadhav@users.noreply.github.com>
2024-01-19 20:40:07 -0500

    Delete .github/workflows directory

--- .github/workflows/numeric_derivation.py
+++
@@ -1,5 +0,0 @@
-
-def derive(f, x, h=0.0001):
-   # return None  # TODO: implement this function 
-    pd = (f(x+h)-f(x))/h
-    return pd
--- .github/workflows/rmse.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def rmse(predictions, targets):
-    pred = np.array(predictions)
-    tar = np.array(targets)
-    #rmse = None # TODO: Implement RMSE Calculation here...
-    n= len(tar)
-    rmse = np.sqrt(np.sum((pred-tar)**2)/n)
-    return rmse
